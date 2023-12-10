219ca058a12d2a70ba777eab6df99453b084d1a3
Federal2JZ <darpanvinodrai@gmail.com>
2023-12-10 20:34:22 +0000

    Defined functions, added meaningful names, main logic placed inside main function

--- milestone_2.py
+++ milestone_2.py
@@ -1,17 +1,37 @@
 import random
 
-word_list = ["Apple", "Banana", "Orange", "Grapes", "Mango"]
-print(word_list)
+def choose_random_word(word_list):
+    return random.choice(word_list)
 
+def get_user_guess():
+    return input("Enter a single letter: ")
 
-word = random.choice(word_list)
-print(word)
+def is_valid_guess(guess):
+    return len(guess) == 1 and guess.isalpha()
 
+def print_feedback(valid_guess):
+    if valid_guess:
+        print("Good guess!")
+    else:
+        print("Oops! That is not a valid input.")
 
-guess = input("Enter a single letter: ")
+def main():
+    # Create a list containing the names of your 5 favorite fruits.
+    favorite_fruits = ["Apple", "Banana", "Orange", "Grapes", "Mango"]
+    
+    # Print out the word list
+    print("Word List:", favorite_fruits)
 
+    # Choose a random word from the list
+    word = choose_random_word(favorite_fruits)
+    print(word)
 
-if len(guess) == 1 and guess.isalpha():
-    print("Good guess!")
-else:
-    print("Oops! That is not a valid input.")
+    # Get user guess
+    guess = get_user_guess()
+
+    # Validate and print feedback
+    valid_guess = is_valid_guess(guess)
+    print_feedback(valid_guess)
+
+if __name__ == "__main__":
+    main()
