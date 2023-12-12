04d178f0e3eecce1a9112e24a12c23410ff9ae2d
Federal2JZ <darpanvinodrai@gmail.com>
2023-12-12 22:06:06 +0000

    Introduced new methods; 'display_result', 'display_current_word', as well as guess handling. Removed unecessary nested loops. tried to reduce the use of 'self' by having a seperate method of 'initialize_game'.

--- milestone_5.py
+++ milestone_5.py
@@ -3,6 +3,9 @@ import random
 class Hangman:
     def __init__(self, word_list, num_lives=5):
         self.word_list = word_list
+        self.initialize_game()
+
+    def initialize_game(self):
         self.word = random.choice(self.word_list).lower()
         self.word_guessed = ['_' for _ in self.word]
         self.num_letters = len(set(self.word))
@@ -13,18 +16,37 @@ class Hangman:
         guess = guess.lower()
 
         if guess in self.word:
-            print(f"Good guess! {guess} is in the word")
-            for i, letter in enumerate(self.word):
-                if letter == guess:
-                    self.word_guessed[i] = guess
-                    self.num_letters -= 1
+            self.handle_correct_guess(guess)
         else:
-            self.num_lives -= 1
-            print(f"Sorry, {guess} is not in the word.")
-            print(f"You have {self.num_lives} lives left.")
+            self.handle_incorrect_guess(guess)
+
+    def handle_correct_guess(self, guess):
+        print(f"Good guess! {guess} is in the word")
+        for i, letter in enumerate(self.word):
+            if letter == guess:
+                self.word_guessed[i] = guess
+                self.num_letters -= 1
+
+        if not '_' in self.word_guessed:
+            self.display_result("Congratulations. You won the game!")
+
+    def handle_incorrect_guess(self, guess):
+        self.num_lives -= 1
+        print(f"Sorry, {guess} is not in the word.")
+        print(f"You have {self.num_lives} lives left.")
+        
+        if self.num_lives == 0:
+            self.display_result(f"You lost! The correct word was: {self.word}")
+
+    def display_current_word(self):
+        print(f"Current word: {' '.join(self.word_guessed)}")
+
+    def display_result(self, message):
+        print(message)
 
     def ask_for_input(self):
         while True:
+            self.display_current_word()
             guess = input("Enter a single letter: ")
 
             if not (len(guess) == 1 and guess.isalpha()):
@@ -34,7 +56,7 @@ class Hangman:
             else:
                 self.list_of_guesses.append(guess)
                 self.check_guess(guess)
-                if self.num_lives == 0 or self.num_letters == 0:
+                if self.num_lives == 0 or not '_' in self.word_guessed:
                     break
 
 def play_game(word_list):
@@ -42,13 +64,8 @@ def play_game(word_list):
     game = Hangman(word_list, num_lives)
 
     while True:
-        if game.num_lives == 0:
-            print(f"You lost!")
-            break
-        elif game.num_letters > 0:
-            game.ask_for_input()
-        else:
-            print("Congratulations. You won the game!")
+        game.ask_for_input()
+        if game.num_lives == 0 or not '_' in game.word_guessed:
             break
 
 word_list = ["apple", "banana", "orange", "grapes", "mango"]
